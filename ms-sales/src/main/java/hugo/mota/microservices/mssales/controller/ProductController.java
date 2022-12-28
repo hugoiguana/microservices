2352f77c4de11940c744c161d9e39a1f5cad358f
hugo iguana <hugo.iguanaa@gmail.com>
2022-12-28 13:06:00 +0000

    Add logs.

--- ms-sales/src/main/java/hugo/mota/microservices/mssales/controller/ProductController.java
+++ ms-sales/src/main/java/hugo/mota/microservices/mssales/controller/ProductController.java
@@ -4,6 +4,7 @@ import hugo.mota.microservices.mssales.dto.ProductDto;
 import hugo.mota.microservices.mssales.entity.ProductEntity;
 import hugo.mota.microservices.mssales.mapper.ProductMapper;
 import hugo.mota.microservices.mssales.service.ProductService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Log4j2
 @RestController
 @RequestMapping("products")
 public class ProductController {
@@ -24,7 +26,7 @@ public class ProductController {
 
     @GetMapping("")
     public ResponseEntity<List<ProductDto>> getAll() {
-
+        log.info("Get all products");
         List<ProductDto> products = service.findAll().stream().map(p -> mapper.toDto(p)).collect(Collectors.toList());
 
         return ResponseEntity.of(Optional.of(products));
@@ -32,7 +34,7 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<ProductDto> create(@RequestBody ProductDto dto) {
-
+        log.info("Create a product");
         ProductEntity entity = mapper.toEntity(dto);
         dto = mapper.toDto(service.create(entity));
 
